eb1060c7e44e50404fa87bab834137fbb7a7bb48
NICKLIN <et20300051@gmail.com>
2024-04-28 23:05:58 +0800

    feat: done Project page roughly

--- lin-portfolio/client/src/App.js
+++ lin-portfolio/client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, CardText } from 'react-bootstrap'
 
 function App() {
@@ -12,11 +12,28 @@ function App() {
       <h1>Hello <span class="badge bg-secondary">New</span> </h1>
 
       <header className="App-header">
+        <h2>About Me</h2>
         <Container className="my-4 bg-secondary text-white">
           <CardText>
-            About Me <br />mike
+            mike
           </CardText>
         </Container>
+
+        <h2>Experience</h2>
+        <Container className="text-start my-4 bg-secondary text-white">
+          Master Student
+          <CardText>
+            <span className="fs-5">University of Illinois Urbana-Champaign, May 2024 - September 2025</span>
+          </CardText>
+        </Container>
+
+        <Container className="text-start my-4 bg-secondary text-white">
+          Undergraduate Student
+          <CardText>
+            <span className="fs-5">Oita University, April 2016 - March 2020</span>
+          </CardText>
+        </Container>
+
       </header>
     </div>
   );
--- lin-portfolio/client/src/pages/ProjectPage.js
+++ lin-portfolio/client/src/pages/ProjectPage.js
@@ -1,11 +1,76 @@
 import React from 'react'
+import '../App.css';
 import Navbar from '../components/Navbar'
+import { Container, CardText } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProjectPage() {
   return (
-    <div>
-        <Navbar />
-        <div>Hello hello!</div>
+    <div className='App'>
+      <Navbar />
+
+      <h1>My Projects <span class="badge bg-secondary">New</span> </h1>
+
+      <header className='App-header'>
+
+        <h1 className='my-4'>2024</h1>
+
+        <div className='container'>
+          <div className="card">
+            <div className='card-image'>
+              <img src="/Project-Images/IMAGE_To-do-list.png" alt="TodoList App" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
+            </div>
+            <div className="card-body">
+              <h4>Todo List</h4>
+              <CardText>
+                <ul className="fs-6 text-start">
+                  <li>Utilized React.js & Express.js to done the full stack work</li>
+                  <li>Integrated MongoDB as a back-end database with RESTful Api</li>
+                  <li>Deployed to render.com and acheived CI</li>
+                </ul>
+              </CardText>
+              <a href="https://lin-todolist.onrender.com" class="btn btn-primary" target="_blank">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className='card-image'>
+              <img src="/Project-Images/Hangman.png" alt="Hangman Game" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
+            </div>
+            <div className="card-body">
+              <h4>Hangman</h4>
+              <CardText>
+                <ul className="fs-6 text-start">
+                  <li>Developed Hangman game using TypeScript</li>
+                  <li>Kept users from entering repeated letters</li>
+                  <li>Generated a touch keyboard</li>
+                </ul>
+              </CardText>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className='card-image'>
+              <img src="/Project-Images/Login-with-Google.png" alt="Login with Google" style={{ width: '286px', height: '256px', objectFit: 'cover', objectPosition: 'center top' }} />
+            </div>
+            <div className="card-body">
+              <h4>Login with Google</h4>
+              <CardText>
+                <ul className="fs-6 text-start">
+                  <li>Integrated Google OAuth 2.0 API</li>
+                  <li>Prevented CSRF attacks with id_token</li>
+                  <li>Protected specific URLs with decorator</li>
+                </ul>
+              </CardText>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+
+        </div>
+
+      </header>
     </div>
   )
 }
